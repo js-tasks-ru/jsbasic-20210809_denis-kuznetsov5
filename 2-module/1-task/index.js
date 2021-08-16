48f4f72eacf3741558bd9a66cb207de3fe0dbb70
Denis Kuznetsov <denispr89@gmail.com>
2021-08-17 00:24:44 +0300

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  if (salaries.isPayed == true) {
+    return 0;
+  }
+  
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  } return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  },
+  
+  sum () {
+    return this.a + this.b;
+  },
+
+  mul () {
+    return this.a * this.b;
+  },
+
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
