faeac510951c7536f2b3d98341d35b30e11ec2ba
Denis Kuznetsov <denispr89@gmail.com>
2021-08-30 12:28:35 +0300

    4-module all exers

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,12 +3,12 @@ function factorial(n) {
   let number = n;
 
   if (n === 0 || n === 1) {
-      return 1;
+    return 1;
   }
 
   while (n > 1) {
-      n--;
-      number = n * number;
+    n--;
+    number = n * number;
   }
 
   return number;
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,52 @@
 function highlight(table) {
-  // ваш код...
+  let row = table.querySelectorAll('tr');
+  let columnStatus;
+  let columnGender;
+  let columnAge;
+
+  for (let i = 0; i < row[0].children.length; i++) {
+    if (row[0].children[i].innerHTML === 'Status') {
+      columnStatus = i;
+    }
+
+    if (row[0].children[i].innerHTML === 'Gender') {
+      columnGender = i;
+    }
+
+    if (row[0].children[i].innerHTML === 'Age') {
+      columnAge = i;
+    }
+  }
+
+  let classContainer = {
+    m: 'male',
+    f: 'female',
+    true: 'available',
+    false: 'unavailable'
+  };
+
+  for (let j = 1; j < row.length; j++) {
+    let classStatus = row[j].children[columnStatus];
+
+    if (classStatus.hasAttributes('data-available')) {
+      let elem = classStatus.getAttribute('data-available');
+      
+      classStatus.parentNode.classList.add(classContainer[elem]);
+    } else {
+      classStatus.parentNode.hidden = true;
+    }
+
+    let classGender = row[j].children[columnGender];
+
+    classGender.parentNode.classList.add(classContainer[classGender.innerHTML]);
+
+    let classAge = row[j].children[columnAge].innerHTML;
+
+    if (classAge < 18) {
+      classGender.parentNode.style.textDecoration = 'line-through';
+    }
+
+  }
+
 }
+
